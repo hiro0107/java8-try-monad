2909a9e925d1ae9fca2190dd73456f55140ef994
hiro <hiro0107@gmail.com>
2014-07-13 10:56:01 +0900

    mvn archetype:generate

---
+++ src/main/java/com/github/hiro0107/trymonad/App.java
@@ -0,0 +1,13 @@
+package com.github.hiro0107.trymonad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
